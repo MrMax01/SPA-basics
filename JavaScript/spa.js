27bd1692b402aab73962d9be53ade4658782dff5
MrMax01 <massimo.mauro00@gmail.com>
2023-07-04 17:03:46 +0200

    update

--- JavaScript/spa.js
+++ JavaScript/spa.js
@@ -1,1 +1,28 @@
+const app = {
+  pages: [],
+  show: new Event("show"),
+  init: () => {
+    app.pages = document.querySelectorAll(".page");
+    app.pages.forEach((pg) => {
+      pg.addEventListener("show", app.pageShown);
+    });
+    document.querySelectorAll("nav-link").forEach((link) => {
+      link.addEventListener("click", app.navigation);
+    });
+    history.replaceState({}, "Home", "#home");
+    window.addEventListener("popstate", app.poppin);
+  },
+  navigation: (event) => {
+    event.preventDefault();
+    let currentPage = event.target.getAttribute("data-target");
+    document.querySelector(".active").classList.remove("active");
+    document.getElementById(currentPage).classList.add("active");
+    history.pushState({}, currentPage, `#${currentPage}`);
+  },
+  pageShown: (event) => {},
+  poppin: (event) => {
+    console.log(location.hash, "popstate event");
+  },
+};
 
+document.addEventListener("DOMContentLoaded", app.init);
