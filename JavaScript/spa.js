01063936e8612a4e05d918bdaf94e25e8dcf609e
MrMax01 <massimo.mauro00@gmail.com>
2023-07-04 15:24:28 +0200

    delete all js and restart

--- JavaScript/spa.js
+++ JavaScript/spa.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //   history.pushState(null, "title", "Home");
-});
 
-// document.addEventListener("click", (event) => {
-//   event.preventDefault();
-// });
-document.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    document.querySelectorAll("div").forEach((box) => {
-      box.classList.remove("active");
-    });
-    let pageName = event.currentTarget.href;
-    console.log(pageName.split("/").pop());
-    document.getElementById(pageName.split("/").pop()).classList.add("active");
-  });
-});
